a4e1966a05d9723c90e75b9758ff8f7cfe41db40
Allen Qiu <allenqiu@Allens-MacBook-Pro.local>
2023-01-21 13:29:32 +0800

    for JS

---
+++ index.js
@@ -0,0 +1,50 @@
+// document.getElementById('count-el').innerText = 5
+// let count = 0
+// console.log(count)
+// let myAge = 40
+// console.log(myAge)
+
+// let myAge = 40
+// let humanDogRatio = 7
+// let myDogAge = myAge*humanDogRatio
+// console.log(myDogAge)
+// let count = 5
+// count = count + 2
+// console.log(count)
+// function increment(){
+// 	console.log("The button was clicked")
+// }
+/*function countdown(){
+	console.log(5)
+	console.log(4)
+	console.log(3)
+	console.log(2)
+	console.log(1)
+}*/
+
+/*
+countdown()
+countdown()*/
+
+
+
+// if (typeof window !== 'undefined') {
+//   console.log('You are on the browser')
+// } else {
+//   console.log('You are on the server')
+// }
+/*global, document*/
+
+
+
+function cook(arg){
+	console.log(arg);
+}
+cook('fish')
+
+function test(n){
+	for(i = 1; i <= 10; i++){
+		console.log('ha' + n);
+	}
+}
+test(3)
